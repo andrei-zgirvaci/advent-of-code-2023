3c60f3a79f07d4517d3646cd6f374443b470fed0
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2023-12-03 18:26:56 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use std::fs::read_to_string;
 
+fn get_numbers_from_line(line: &str) -> Vec<u32> {
+    const NUMBER_STRINGS: [&str; 9] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let mut numbers: Vec<u32> = Vec::new();
+
+    for character in line.chars() {
+        if let Some(digit) = character.to_digit(10) {
+            numbers.push(digit);
+            println!("{}", digit);
+        }
+    }
+
+    return numbers;
+}
+
 fn first_1() {
     const FILE_PATH: &str = "src/1_input";
 
@@ -14,20 +31,12 @@ fn first_1() {
     for line in input_lines {
         println!("{}", line);
 
-        let mut numbers: Vec<u32> = Vec::new();
-        for character in line.chars() {
-            if let Some(digit) = character.to_digit(10) {
-                numbers.push(digit);
-                println!("{}", digit);
-            }
-        }
+        let numbers: Vec<u32> = get_numbers_from_line(line);
 
-        let mut number: u32 = 0;
-        if let Some(first_digit) = numbers.first() {
-            let last_digit = numbers.last().unwrap();
+        let first_digit = numbers.first().unwrap_or(&0);
+        let last_digit = numbers.last().unwrap_or(&0);
 
-            number = first_digit * 10 + last_digit;
-        }
+        let number: u32 = first_digit * 10 + last_digit;
 
         println!("number: {}", number);
 
@@ -35,12 +44,6 @@ fn first_1() {
     }
 
     println!("sum: {}", sum);
-}
-
-fn first_2() {
-    const FILE_PATH: &str = "src/1_input";
-
-    let contents: String = read_to_string(FILE_PATH).unwrap();
 }
 
 fn main() {
